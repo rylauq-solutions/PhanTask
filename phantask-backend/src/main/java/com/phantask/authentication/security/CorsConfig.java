c5c646f0da7dc767bd6babb872b89e0bdf194a14
Anubhav Goel <anubhavgoel0520@hotmail.com>
2025-12-02 21:13:42 +0530

    feat: update CORS configuration to include deployed frontend URL

--- phantask-backend/src/main/java/com/phantask/authentication/security/CorsConfig.java
+++ phantask-backend/src/main/java/com/phantask/authentication/security/CorsConfig.java
@@ -13,7 +13,11 @@ public class CorsConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:5173", "http://localhost:3000")
+				registry.addMapping("/**").allowedOrigins(
+                        "http://localhost:5173", 
+                        "http://localhost:3000",
+                        "https://phantask.vercel.app" // deployed frontend
+                    )
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
 						.allowCredentials(true);
 			}
